8c29c7d120d901af8eea01af5edb188e2e08b48d
Ganesh-010 <venkatganesh010@gmail.com>
2025-03-24 14:08:10 +0530

    intial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './component/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div><h1>Hello Javascript </h1></div>
+    <div><h1>Hello Javascript </h1>
+    <Home/>
+    </div>
   )
 }
 
